7ab5f9346e7387ef12e4af3f87eac54dc5809345
HenryKasai <henrykasai1775@gmail.com>
2020-04-29 22:27:44 -0300

    Exercício resolvido.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Insira uma temperatura em Fahrenheit e veja a mesma em Celsius.");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string f = Console.ReadLine();
+            decimal f1 = Convert.ToDecimal(f);
+
+            string um = "1,8";
+            decimal um1 = Convert.ToDecimal(um);
+            decimal result1 = f1 - 32;
+            decimal resultfinal = result1 / um1;
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Seu resultado em Celsius é " + resultfinal + "°C.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
